f9d289667f83216364fc22f1d37653b9ede3d9c4
codewitch-honey-crisis <gaztoast@gmail.com>
2022-12-22 01:18:39 -0800

    changed how RPM adj works

--- include/rpm_controller.hpp
+++ include/rpm_controller.hpp
@@ -10,6 +10,7 @@ class rpm_controller final {
     uint8_t m_input_pin;
     uint8_t m_output_pin;
     uint32_t m_ts;
+    int m_state;
     unsigned int m_change_delay_ms;
     unsigned int m_rpm;
     volatile int m_count;
@@ -29,6 +30,7 @@ class rpm_controller final {
         m_input_pin = rhs.m_input_pin;
         m_output_pin = rhs.m_output_pin;
         m_ts = rhs.m_ts;
+        m_state = rhs.m_state;
         m_change_delay_ms = rhs.m_change_delay_ms;
         m_rpm = rhs.m_rpm;
         m_count = rhs.m_count;
@@ -56,56 +58,64 @@ public:
         do_move(rhs);
         return *this;
     }
-    rpm_controller(uint8_t input_pin,uint8_t output_pin, uint32_t max_rpm=6500,uint32_t change_delay_ms=500,uint8_t channel = 0,unsigned int frequency=25*1000,uint8_t resolution=8) : m_initialized(false), m_input_pin(input_pin),m_output_pin(output_pin) ,m_ts(0),m_change_delay_ms(change_delay_ms), m_rpm(0), m_count(0),m_channel(channel),m_resolution(resolution),m_frequency(frequency),m_max_rpm(max_rpm),m_target_rpm(0),m_target_rpm_adj(0) {
+    rpm_controller(uint8_t input_pin,uint8_t output_pin, uint32_t max_rpm=6500,uint32_t change_delay_ms=500,uint8_t channel = 0,unsigned int frequency=25*1000,uint8_t resolution=8) : m_initialized(false), m_input_pin(input_pin),m_output_pin(output_pin) ,m_ts(0), m_state(-1),m_change_delay_ms(change_delay_ms), m_rpm(0), m_count(0),m_channel(channel),m_resolution(resolution),m_frequency(frequency),m_max_rpm(max_rpm),m_target_rpm(0),m_target_rpm_adj(0) {
     }
     ~rpm_controller() {
-        if(m_initialized) {
+        if(initialized()) {
             detachInterrupt(digitalPinToInterrupt(m_input_pin));
             ledcDetachPin(m_output_pin);
         }
     }
     void initialize() {
-        if(!m_initialized) {
+        if(m_state==-1) {
             m_rpm = 0;
             m_target_rpm = 0;
             m_ts = millis();
+            m_state = 0;
             m_count = 0;
             m_target_rpm_adj = 0;
             m_target_ts = 0;
             attachInterruptArg(digitalPinToInterrupt(m_input_pin),handler,this,RISING);
             ledcSetup(m_channel,m_frequency,m_resolution);
             ledcAttachPin(m_output_pin,m_channel);
-            ledcWrite(m_channel,0);
-            m_initialized = true;
+            if(m_max_rpm==0) {
+                m_state = -2;
+                m_target_ts = m_ts;
+            } else {
+                ledcWrite(m_channel,0);
+            }
         }
     }
+    bool initialized() const {
+        return m_state!=-1;
+    }
     unsigned int rpm() const {
-        if(m_initialized && m_ts!=0) {
+        if(initialized() && m_ts!=0) {
             return m_rpm;
         }
         return 0;
     }
     unsigned int max_rpm() const {
-        if(m_initialized) {
+        if(initialized()) {
             return m_max_rpm;
         }
         return 0;
     }
     void rpm(unsigned int rpm) {
-        if(m_initialized) {
-            if(rpm>m_max_rpm) {
-                rpm=m_max_rpm;
+        if(initialized()) {
+            if(rpm>m_rpm) {
+                m_state = 1;
                 m_target_rpm = rpm;
-                m_target_rpm_adj = 0;
-                int max = (1<<m_resolution)-1;
-                int val = ((float)m_target_rpm/(float)m_max_rpm)*max;
-                ledcWrite(m_channel,val);
+                m_target_ts = millis();
+            } else if(rpm<m_rpm) {
+                m_state = 2;
+                m_target_rpm = rpm;
                 m_target_ts = millis();
             }
         }
     }
     void update() {
-        if(m_initialized && m_ts>0) {
+        if(initialized() && m_ts>0) {
             uint32_t ms = millis();
             float msecs = (ms-m_ts);
             unsigned int old = m_count;
@@ -116,34 +126,67 @@ public:
                 m_count -=old;
                 m_ts += 60000;
             }
-            if(m_target_rpm!=m_rpm) {
-                if(m_ts+m_change_delay_ms<=ms) {
-                    int max = (1<<m_resolution)-1;
-                    int rpm_delta = m_max_rpm/(1<<m_resolution);
-                    int val = ((float)m_target_rpm/(float)m_max_rpm)*max;
-                    if(m_rpm>m_target_rpm) {
-                        int cmp = m_rpm+rpm_delta;
-                        if(m_target_rpm-cmp<m_rpm-m_target_rpm) {
+            switch(m_state) {
+                case -2: // autodetect max rpm
+                    if(m_rpm==0) {
+                        ledcWrite(m_channel,(1<<m_resolution)-1);
+                    }
+                    // give time for spinup
+                    if(ms-m_target_ts>=(m_change_delay_ms*5) && m_rpm!=0) {
+                        m_max_rpm = m_rpm;
+                        m_state = 0;
+                    }
+                    break;
+                case 0: // nothing needed
+                    break;
+                case 1: // changing up (target RPM > RPM)
+                    if(ms-m_target_ts>=m_change_delay_ms) {
+                        if(m_rpm>m_target_rpm) {
                             --m_target_rpm_adj;
-                        }
-                    } else {
-                        int cmp = m_rpm-rpm_delta;
-                        if(cmp-m_target_rpm<m_target_rpm-m_rpm) {
+                            if(m_target_rpm_adj<0) {
+                                m_target_rpm_adj = 0;
+                            }
+                            ledcWrite(m_channel,(uint32_t)m_target_rpm_adj);
+                            m_state = 0;
+                        } else if(m_rpm<m_target_rpm) {
                             ++m_target_rpm_adj;
+                            uint32_t max = (1<<m_resolution)-1;
+                            if(m_target_rpm_adj>max) {
+                                m_target_rpm_adj = max;
+                                m_state = 0;
+                            }
+                            ledcWrite(m_channel,(uint32_t)m_target_rpm_adj);
+                        } else {
+                            m_state = 0;
                         }
+                        m_target_ts = millis();
                     }
-                    val+= m_target_rpm_adj;    
-                    if(val<0) {
-                        val = 0;
-                    } else if(val>max) {
-                        val = max;
+                    break;
+                case 2: // changing down (target RPM < RPM)
+                    if(ms-m_target_ts>=m_change_delay_ms) {
+                        if(m_rpm<m_target_rpm) {
+                            uint32_t max = (1<<m_resolution)-1;
+                            ++m_target_rpm_adj;
+                            if(m_target_rpm_adj>=max) {
+                                m_target_rpm_adj = max;
+                            }
+                            ledcWrite(m_channel,(uint32_t)m_target_rpm_adj);
+                            m_state = 0;
+                        } else if(m_rpm>m_target_rpm) {
+                            --m_target_rpm_adj;
+                            if(m_target_rpm_adj<0) {
+                                m_target_rpm_adj = 0;
+                                m_state = 0;
+                            }
+                            ledcWrite(m_channel,(uint32_t)m_target_rpm_adj);
+                        } else {
+                            m_state = 0;
+                        }
+                        m_target_ts = millis();
                     }
-                    ledcWrite(m_channel,val);
-                    m_ts = millis();
-                }
-                
-
+                    break;
             }
+        
         }
     }
 };
